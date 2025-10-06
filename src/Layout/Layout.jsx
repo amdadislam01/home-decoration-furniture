7b7267bb84708efbde4343636ef45bdcbd4c7915
amdadislam01 <amdadislam733@gmail.com>
2025-10-06 16:09:50 +0600

    cart added complate

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import { Outlet } from 'react-router'
-import Footer from '../components/Footer'
-import { CartProvider } from '../Context/CartContext'
-import { AuthProvider } from '../Context/AuthContext' // ✅ Import AuthProvider
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../components/Footer";
+import { CartProvider } from "../Context/CartContext";
 
 const Layout = () => {
-    return (
-        <AuthProvider> {/* ✅ Wrap with AuthProvider */}
-            <CartProvider>
-                <div className='flex flex-col min-h-screen'>
-                    <Navbar />
-                    <div className="flex-1">
-                        <Outlet />
-                    </div>
-                    <Footer />
-                </div>
-            </CartProvider>
-        </AuthProvider>
-    )
-}
+  return (
+    <CartProvider>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex-1">
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
+    </CartProvider>
+  );
+};
 
-export default Layout
+export default Layout;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router"; // ✅ react-router-dom
 
 import Layout from "../Layout/Layout";
 import Home from "../pages/Home";
@@ -10,64 +10,26 @@ import ProductDetails from "../pages/ProductDetails";
 import Cart from "../pages/Cart";
 import Payment from "../pages/Payment";
 import OrderSuccess from "../pages/OrderSuccess";
-import PrivateRoute from "../components/PrivateRoute";
-import Login from "../pages/Login";
 import Orders from "../pages/Orders";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/product",
-        element: <Products />,
-      },
-      {
-        path: "/product/:id",
-        element: <ProductDetails />,
-      },
-      {
-        path: "/whislist",
-        element: <Whislist />,
-      },
-      {
-        path: "/cart",
-        element: (
-          <PrivateRoute>
-            <Cart />
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: "/payment",
-        element: (
-          <PrivateRoute>
-            <Payment />
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: "/order-success",
-        element: (
-          <PrivateRoute>
-            <OrderSuccess />
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: "/orders",
-        element: (
-          <PrivateRoute>
-            <Orders />
-          </PrivateRoute>
-        ),
-      },
-    //   { path: "/login", element: <Login /> },
+      { index: true, element: <Home /> },
+      { path: "/product", element: <Products /> },
+      { path: "/product/:id", element: <ProductDetails /> },
+      { path: "/whislist", element: <Whislist /> },
+
+      // Cart accessible without login
+      { path: "/cart", element: <Cart /> },
+
+      // Payment, OrderSuccess, Orders - Clerk will handle login redirect
+      { path: "/payment", element: <Payment /> },
+      { path: "/order-success", element: <OrderSuccess /> },
+      { path: "/orders", element: <Orders /> },
     ],
   },
 ]);
